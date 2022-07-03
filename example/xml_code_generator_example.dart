b385429fd37679e687c368c59dede36452e22757
Ihar Kukharchuk <Ihar_Kukharchuk@epam.com>
2022-07-02 23:54:48 -0400

    Initialize default project

---
+++ example/xml_code_generator_example.dart
@@ -0,0 +1,6 @@
+import 'package:xml_code_generator/xml_code_generator.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/xml_code_generator.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library xml_code_generator;
+
+export 'src/xml_code_generator_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
