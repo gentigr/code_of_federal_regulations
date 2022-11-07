342441282d5d636ad530538183276dfe5ed15405
Ihar Kukharchuk <Ihar_Kukharchuk@epam.com>
2022-11-06 22:48:46 -0500

    Export Regulation Unit from package

--- lib/code_of_federal_regulations.dart
+++ lib/code_of_federal_regulations.dart
@@ -4,5 +4,6 @@
 library code_of_federal_regulations;
 
 export 'src/code_of_federal_regulations.dart';
+export 'src/regulation_unit.dart';
 
 // TODO: Export any libraries intended for clients of this package.
